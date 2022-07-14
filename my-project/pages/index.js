07f45fe61468f697ddf3644b70040d2af037dea1
Nef <maximenef1@gmail.com>
2022-07-14 11:23:06 +0200

    install NextJs Tailwindcss Framer motion

---
+++ my-project/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
+export default function Home() {
+  return (
+    <div className=''>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
+}
